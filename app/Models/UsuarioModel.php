586700c90444a97078d869dbdf065becb99f1b37
allanranier <allanranier18@gmail.com>
2021-07-19 16:43:12 -0300

    validação no model usuario

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -8,9 +8,34 @@ class UsuarioModel extends Model
 {
 	protected $table = 'usuarios';
 	protected $returnType = 'App\Entities\Usuario';
-	protected $useSoftDelete = true;
 	protected $allowedFields = ['nome','email','telefone', 'password_hash'];
+	protected $useSoftDelete = true;
+	protected $useTimestamps = true;
+    protected $createdField  = 'criado_em';
+    protected $updatedField  = 'atualizado_em';
+    protected $deletedField  = 'deletado_em';
 
+	protected $validationRules    = [
+        'nome'     => 'required|min_length[4]|min_length[120]',
+        'email'        => 'required|valid_email|is_unique[usuario.email]',
+        'cpf'        => 'required|exact_length[14]|is_unique[usuario.cpf]',
+        'password'     => 'required|min_length[6]',
+        'pass_confirm' => 'required_with[password]|matches[password]'
+    ];
+
+    protected $validationMessages = [
+        'nome'        => [
+            'required' => 'Esse campo é obrigátorio.',
+		],
+        'email'        => [
+            'required' => 'Esse campo é obrigátorio.',
+            'is_unique' => 'Desculpe. Esse e-mail já existe.',
+		],
+        'cpf'        => [
+            'required' => 'Esse campo é obrigátorio.',
+            'is_unique' => 'Desculpe. Esse CPF já existe.',
+		],
+    ];
 
 	/**
 	 *  Array de Usuarios
